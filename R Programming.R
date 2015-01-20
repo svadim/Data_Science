11ef351ab85be4f16d9a1089241957d055159166
Sokolov Vadim <vadim@engineer.com>
2015-01-20 19:53:43 +0300

    week 3

--- R Programming.R
+++ R Programming.R
@@ -1,5 +1,47 @@
 # Week 3. -----------------------------------------------------------------
 
+      #split
+
+x <- c(rnorm(10), runif(10), rnorm(10,1))
+f <- gl(3, 10)
+split(x, f)
+lapply(split(x, f), mean)
+
+library(datasets)
+head(airquality)
+s <- split(airquality, airquality$Month)
+lapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")]))
+sapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")]))
+sapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")], na.rm=TRUE))
+
+x <- rnorm(10)
+f2 <- gl(5, 2)
+f1 <- gl(2, 5)
+interaction(f1, f2)
+str(split(x, list(f1, f2)))
+str(split(x, list(f1, f2), drop=TRUE))
+
+      #tapply
+
+x <- c(rnorm(10), runif(10), rnorm(10,1))
+f <- gl(3, 10)
+tapply(x, f, mean)
+tapply(x, f, range)
+
+      #mapply
+
+l1 <- list(rep(1, 4), rep(2, 3), rep(3, 2), rep(4, 1))
+l2 <- mapply(rep, 1:4, 4:1)
+
+noise <- function(n, mean, sd) {
+      rnorm(n, mean, sd)
+}
+noise(5, 1, 2)
+noise(1:5, 1:5, 2)
+mapply(noise, 1:5, 1:5, 2)
+
+      # lapply, apply
+
 x <- list(a = 1:5, b = rnorm(10))
 lapply(x, mean)
 
