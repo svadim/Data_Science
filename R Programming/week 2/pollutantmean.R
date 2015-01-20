c093c69f8d476f13dd377337ad667b473e1b21b3
Vadim Sokolov <vadim@engineer.com>
2015-01-20 17:06:47 +0300

    week 3

--- R Programming/week 2/pollutantmean.R
+++ R Programming/week 2/pollutantmean.R
@@ -1,3 +1,6 @@
+
+# vers. 1 -----------------------------------------------------------------
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
         files_list <- list.files(directory, full.names = TRUE)[id]
         data <- data.frame()
@@ -6,3 +9,20 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         }
         mean(data[, pollutant], na.rm = TRUE)
 }
+
+# vers. 2 -----------------------------------------------------------------
+
+pollutantmean <- function(path, pollutant, id){
+      flist <- list.files(path, full.names=TRUE)[id]
+      data <- lapply(flist, read.csv)
+      p_data <- sapply(data, function(data)
+            data[, pollutant])
+      p_data
+      #mean(p_data, na.rm=TRUE)
+}
+
+path <- "R Programming/week 2/specdata"
+id <- 1:3
+pollutant <- "sulfate"
+
+m1 <- pollutantmean(path, pollutant, id)
