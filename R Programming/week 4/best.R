5fd46433145d2b02c603e7c5ed3fcdb134b4dbb5
Vadim Sokolov <vadim@engineer.com>
2015-02-02 11:56:21 +0300

    R Programming completed

--- R Programming/week 4/best.R
+++ R Programming/week 4/best.R
@@ -21,3 +21,26 @@ best <- function(state, outcome) {
       hos_name <- hospital[na][min]
       sort(hos_name)[1]
 }
+
+best2 <- function(state, outcome) {
+      
+#       Read outcome data
+      data <- read.csv("outcome-of-care-measures.csv", colClasse ="character")
+      colnames(data)[11] <- "heart attack"
+      colnames(data)[17] <- "heart failure"
+      colnames(data)[23] <- "pneumonia"
+      
+#       Check that state and outcome are valid
+      state_all <- unique(data$State)
+      outcome_all <- c("heart attack", "heart failure", "pneumonia")
+      if(sum(state_all == state) == 0) stop("invalid state")
+      if(sum(outcome_all == outcome) == 0) stop("invalid outcome")
+      
+#       Return hospital name in that state with lowest 30-day death rate
+      data <- data[data$State == state, ]
+      rate <- as.numeric(data[, outcome])
+      data <- data[with(data, order(rate, Hospital.Name)), ]
+      data[1, "Hospital.Name"]
+}
+
+c <- best2("MD", "heart attack")
--- R Programming/week 4/rankhospital.R
+++ R Programming/week 4/rankhospital.R
@@ -22,3 +22,30 @@ rankhospital <- function(state, outcome, num = "best") {
       if(num == "worst") num <- sum(!is.na(rate))
       hos_rate[order(rate, hospitals), ][num]
 }
+
+rankhospital2 <- function(state, outcome, num = "best") {
+      
+      #       Read outcome data
+      data <- read.csv("outcome-of-care-measures.csv", colClasse ="character")
+      colnames(data)[11] <- "heart attack"
+      colnames(data)[17] <- "heart failure"
+      colnames(data)[23] <- "pneumonia"
+      
+      #       Check that state and outcome are valid
+      state_all <- unique(data$State)
+      outcome_all <- c("heart attack", "heart failure", "pneumonia")
+      if(sum(state_all == state) == 0) stop("invalid state")
+      if(sum(outcome_all == outcome) == 0) stop("invalid outcome")
+      
+      #       Return hospital name in that state with given rank
+      #       30-day death rate
+      s <- split(data, data$State)
+      rank <- lapply(s, function(x) {
+            rate <- as.numeric(x[, outcome])
+            x <- x[with(x, order(rate, Hospital.Name)), ]
+            if(num == "best") num <- 1
+            if(num == "worst") num <- sum(!is.na(rate))
+            x[num, "Hospital.Name"]
+      })
+      do.call(rbind, rank)
+}
