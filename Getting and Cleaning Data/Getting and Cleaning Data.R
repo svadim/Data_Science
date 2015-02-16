5bb2ba12bbb2c7bbde83d5520a4230539b799b3d
Sokolov Vadim <vadim@engineer.com>
2015-02-16 21:32:23 +0300

    Week 3

--- Getting and Cleaning Data/Getting and Cleaning Data.R
+++ Getting and Cleaning Data/Getting and Cleaning Data.R
@@ -1,4 +1,66 @@
 
+# Week 3 ------------------------------------------------------------------
+
+##Summarizing data ************************************************************
+if(!file.exists("data")) dir.create("data")
+url <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(url, "data/restaurants.csv", mode = "wb")
+restData <- read.csv("data/restaurants.csv")
+head(restData, n=3)
+tail(restData, n=3)
+summary(restData)
+str(restData)
+quantile(restData$councilDistrict, na.rm = T)
+quantile(restData$councilDistrict, probs=c(0.5, 0.75, 0.9))
+table(restData$zipCode, useNA = "ifany")
+table(restData$councilDistrict, restData$zipCode)
+sum(is.na(restData$councilDistrict))
+any(is.na(restData$councilDistrict))
+all(restData$zipCode > 0)
+colSums(is.na(restData))
+all(colSums(is.na(restData)) == 0)
+table(restData$zipCode %in% c("21212", "21213"))
+restData[restData$zipCode %in% c("21212", "21213"),]
+
+data(UCBAdmissions)
+DF <- as.data.frame(UCBAdmissions)
+summary(DF)
+xt <- xtabs(Freq ~ Gender + Admit, data=DF)
+ftable(xt)
+
+warpbreaks$replicate <- rep(1:9, len = 54)
+xt <- xtabs(breaks ~.,data=warpbreaks)
+
+fakeData = rnorm(1e5)
+object.size(fakeData)
+print(object.size(fakeData),units="Mb")
+
+##Subsetting and sorting ******************************************************
+set.seed(13435)
+X <- data.frame("var1"=sample(1:5), "var2"=sample(6:10), "var3"=sample(11:15))
+X <- X[sample(1:5),]; X$var2[c(1,3)]=NA
+
+X[,1]
+X[,"var1"]
+X[1:2,"var2"]
+
+X[(X$var1 <= 3 & X$var3 > 11), ]
+X[(X$var1 <= 3 | X$var3 > 11), ]
+X[which(X$var2 > 8),]
+
+sort(X$var1)
+sort(X$var3, decreasing = T)
+sort(X$var2, na.last = T)
+
+X[order(X$var1),]
+X[order(X$var1, X$var3), ]
+
+library(plyr)
+arrange(X, var1)
+arrange(X, desc(var1))
+X$var4 <- rnorm(5)
+Y <- cbind(X, rnorm(5))
+
 # Week 2 ------------------------------------------------------------------
       
 ##Quiz*************************************************************************
